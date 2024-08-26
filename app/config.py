9eb31c7495fd0c4cedb707ed526bcd0d5602eb2a
duyvo26 <duyvo1945@gmail.com>
2024-08-27 01:14:09 +0700

    upCode Base

--- app/config.py
+++ app/config.py
@@ -8,15 +8,9 @@ load_dotenv()
 
 
 class Settings:
-    # FLASK SETTING
-    SITE_APP = os.environ["SITE_APP"]
-    DEBUG = True
-    
     # SETTING
     DIR_ROOT = os.path.dirname(os.path.abspath(".env"))
 
-    # FAST API
-    API_KEY = os.environ["API_KEY"]
 
 
 settings = Settings()
